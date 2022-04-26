fba899a7c58a0816604aba5dec9ed429375a33bb
SydVic <fabrizio.bocca27@gmail.com>
2022-04-26 14:37:40 +0200

    aggiunta funzione per generare l'array contenete i numeri casuali per posizionamento bombe

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@ const playBtn = document.getElementById("play-game");
 
 playBtn.addEventListener("click", function() {
 
+    // SELEZIONE DELLA DIFFICOLTA' E ASSEGNAZIONE GRIGLIA 
     const userDifficulty = document.getElementById("user-difficulty").value;
-    console.log(userDifficulty);
 
     let boxQuantity = 0;
     let boxSize = "";
@@ -19,13 +19,20 @@ playBtn.addEventListener("click", function() {
         boxSize = "crazy";
     }
 
+    // GENERAZIONE RANDOM BOMBE
+    const bombsQuantity = 16;
+    const maxBoxQuantity = boxQuantity;
+    const bombsPlacement = generateUniqueRndNumbers(bombsQuantity, maxBoxQuantity);
+    console.log(bombsPlacement);
+
+
+    // CREAZIONE ELEMENTI GRIGLIA
     const gridContainer = document.querySelector(".grid-container");
     gridContainer.innerHTML = "";
     for ( let i = 1; i <= boxQuantity; i++ ) {
     
         // assegno il numero al box
         const boxNumber = i;
-        console.log(boxNumber);
         
         // creo l'elemento in html con il numero al suo interno
         const gridBox = document.createElement("div");
@@ -44,3 +51,30 @@ playBtn.addEventListener("click", function() {
         gridContainer.append(gridBox);
     }
 })
+// generare 16 numeri diversi uno dall'altro casuali nello stesso range dei numeri della griglia (100 7 81 / 49)
+    // array in cui salvare i numeri delle bombe
+    // piazzare le "bombe" in corrispondenzza dei 16 numeri
+
+// FUNCTION
+
+/**
+ * Description generate X unique random number with a max limit range and create an array containing them
+ * @param {Number} numberQuantity -> quantity of number you want to generate
+ * @param {Number} maxLimit -> max value the number can get
+ * @returns {Array} -> the array with the random unique numbers
+ */
+function generateUniqueRndNumbers(numberQuantity, maxLimit) {
+    const randomsUniqueNumbersArray = [];
+    while (randomsUniqueNumbersArray.length < numberQuantity) {
+        const randomNumber = getRndInteger(1, maxLimit);
+        if ( !randomsUniqueNumbersArray.includes(randomNumber)) {
+            randomsUniqueNumbersArray.push(randomNumber);
+        }
+    }
+    return randomsUniqueNumbersArray;
+}
+
+// UTILITY FUNCTION
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
